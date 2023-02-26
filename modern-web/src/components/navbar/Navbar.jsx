46a1660e3888bef64f87c20f77811a54f9d10d8e
Matias Gonzalez <mng_epson@hotmail.com>
2023-02-26 08:56:20 -0300

    feat: add assets folder

--- modern-web/src/components/navbar/Navbar.jsx
+++ modern-web/src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './navbar.css';
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
